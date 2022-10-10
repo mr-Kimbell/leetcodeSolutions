f547dc49996d96c36c1911d585a543a533b76fd2
Pratham8979 <115494385+Pratham8979@users.noreply.github.com>
2022-10-10 23:10:43 +0530

    Create 852.cpp

---
+++ 852.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int l=0, h=arr.size()-1;
+        int mid = l + (h-l)/2;
+        
+        while(l<h){
+            if(arr[mid]< arr[mid+1]){
+                l=mid+1;
+            }
+            else{
+                h=mid;
+            }
+            mid = l + (h-l)/2;
+        }
+    return l;
+
+    }
+};
